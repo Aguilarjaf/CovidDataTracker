6cae8e9dc574b4d4aee3cfc0a477b321d25e9aea
Jorge Aguilar <Jorge.Aguilar4120@gmail.com>
2021-07-31 14:40:47 -0700

    Add footer component, update CSS to style footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import StateSearch from "./StateSearch.jsx";
 import LearnMore from "./LearnMore.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
   return (
     <div>
       <StateSearch />
       {/* <LearnMore /> */}
+      <Footer />
     </div>
   );
 }
